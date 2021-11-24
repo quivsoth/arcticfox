7c407d37df1404b397ff042f7205aeb72f9a9a0d
Phillip Knezevich <phillip.knezevich@gmail.com>
2021-11-24 18:01:59 -0500

    ocp test

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
 
-// Connect to the db
-MongoClient.connect("mongodb://mongodb-26-rhel7.mle.srv.cluster.local:27017/shop", function (err, db) {
-   
-     if(err) {
-         console.log("There was an error connecting : " + err);
-         throw err;
-     } else {
-        console.log("Connected");
-     }
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 
-     //Write databse Insert/Update/Query code here..
-            
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+ 
+
+server.listen(server_port, server_ip_address, function () {
+
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+
 });
+
+
+// // Connect to the db
+// MongoClient.connect("mongodb://mongodb-26-rhel7.mle.srv.cluster.local:27017/shop", function (err, db) {
+   
+//      if(err) {
+//          console.log("There was an error connecting : " + err);
+//          throw err;
+//      } else {
+//         console.log("Connected");
+//      }
+
+//      //Write databse Insert/Update/Query code here..
+            
+// });
