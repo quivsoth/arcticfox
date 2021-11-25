3c42969d0005f21cd8e9eadc9461fb4843a299fc
quivsoth <phillip.knezevich@gmail.com>
2021-11-25 14:43:41 -0500

    trying to add mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const {MongoClient} = require('mongodb');
+var mongoose = require("mongoose");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  (async function() {
+    const collections = await getCollections();
+    res.render('index', { title: 'Express' });
+  })();
 });
 
 module.exports = router;
+
+
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-* DB FUNCTIONS -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*//
+async function getCollections(){
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  try {
+      await client.connect();
+      const cursor = client.db("shop").collection("bruja").find({active: true}).sort({collectionId:1});
+      const results = await cursor.toArray();
+      return results;
+  } catch (e) { console.error(e); }
+  finally { await client.close(); }
+}
