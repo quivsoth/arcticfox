4baa8bc54142dec0539826d3663416c36d18c4ca
quivsoth <phillip.knezevich@gmail.com>
2021-12-04 15:18:51 -0500

    s2i logic added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
+console.log("Starting Arctic Fox");
+console.log("DB: " + process.env.MONGO_DB);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
